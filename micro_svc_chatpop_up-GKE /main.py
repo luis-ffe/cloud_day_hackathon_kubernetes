cba8c42e754037a7a773452b810e59b5c81b62fb
luis-ffe <filip.carvalho12@gmail.com>
2024-06-25 16:08:16 +0100

    ok

---
+++ micro_svc_chatpop_up-GKE /main.py
@@ -0,0 +1,16 @@
+
+#MEGABOSSES@42Porto
+
+import os, uuid, datetime, pytz
+
+from flask import Flask, render_template, request, redirect, url_for
+from google.cloud import vision, translate, storage, datastore
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
